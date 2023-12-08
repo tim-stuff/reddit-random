c43275f0730059d8812bcb2f96f157d2184f54cb
kror-shack <tariqfouzan1@gmail.com>
2023-12-08 14:49:00 +0300

    feat: Add SearchBar component

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { indentifySearchInputType } from "@/utils/HelperFunctions/HelperFunctions";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+
+type Props = {
+  header?: boolean;
+};
+
+/**
+ * Renders a search bar which redirects to the relevant page depending on the input type
+ * Accepts addresses, transaction hash & tokens
+ *
+ * @param props - The searchbar props
+ * @param props.header - a boolean for whether the component is mounted on the header
+ * @returns The search bar
+ */
+const SearchBar = ({ header = false }: Props) => {
+  const [searchInput, setSearchInput] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const router = useRouter();
+  const pathName = usePathname();
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const type = indentifySearchInputType(searchInput);
+    if (!type) {
+      setErrorMsg("Please enter a valid input.");
+      return;
+    }
+    router.push(`/${type}/${encodeURIComponent(searchInput)}`);
+  }
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (errorMsg) setErrorMsg("");
+    setSearchInput(event.target.value);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={`md:w-full max-w-3xl ${
+        header && pathName === "/" && "hidden"
+      }`}
+    >
+      <label
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      >
+        Search
+      </label>
+      <div className="flex gap-2 border-2 p-2 rounded-md">
+        <input
+          type="search"
+          id="default-search"
+          className=" p-2  text-sm text-gray-900 rounded-lg focus:outline-none focus:border-2-purple w-full "
+          placeholder="Search for a particular address, transaction hash or token"
+          value={searchInput}
+          onChange={handleInputChange}
+          required
+        />
+        <button
+          type="submit"
+          aria-label="Search"
+          className="text-white p-1 h-fit  bg-purple-500 hover:bg-purple-600  font-medium rounded-lg text-sm  "
+        >
+          <Image src="/search.svg" width={30} height={30} alt="Search-icon" />
+        </button>
+      </div>
+      {errorMsg && <p className=" text-xs text-red-600 ">{errorMsg}</p>}
+    </form>
+  );
+};
+
+export default SearchBar;
---
+++ src/utils/HelperFunctions/HelperFunctions.ts
@@ -0,0 +1,47 @@
+/**
+ * Formats a Unix timestamp into a human-readable date string.
+ *
+ * @param timestamp The Unix timestamp to be formatted.
+ * @returns The formatted date string.
+ */
+export function formatUnixTime(unixTimestamp: number): string {
+  const date = new Date(unixTimestamp * 1000);
+  const options: Intl.DateTimeFormatOptions = {
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
+  return `${formattedDate} `;
+}
+
+/**
+ * Determines the type of input based on predefined patterns.
+ *
+ * @param input - the input string to be identified
+ * @returns The type of the input string or null if it does not match any.
+ */
+export function indentifySearchInputType(input: string) {
+  // Check if the input matches an Ethereum address pattern
+  const addressPattern = /^(0x)?[0-9a-fA-F]{40}$/;
+  if (addressPattern.test(input)) {
+    return "address";
+  }
+
+  // Check if the input matches an Ethereum transaction hash pattern
+  const txHashPattern = /^(0x)?[0-9a-fA-F]{64}$/;
+  if (txHashPattern.test(input)) {
+    return "hash";
+  }
+
+  // Check if the input matches an ERC-20 token address pattern (example pattern)
+  const tokenPattern = /^(0x)?[0-9a-fA-F]{40}$/;
+  if (tokenPattern.test(input)) {
+    return "token";
+  }
+
+  return null;
+}
